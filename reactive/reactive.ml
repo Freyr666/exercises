8d002d2dbd64d24a566ce5336671ba41de3eb250
Freyr <sky_rider_93@mail.ru>
2017-04-12 17:22:15 +0300

    lwt stuff

--- reactive/reactive.ml
+++ reactive/reactive.ml
@@ -5,13 +5,18 @@ let pr_time t =
 
 open Lwt_react;;
 
-let seconds, run =
-  let e, send = E.create () in
-  let run () =
-    while true do send (Unix.gettimeofday ()); Unix.sleep 1 done
+let create fsend frecv interval =
+  let seconds, run =
+    let e, send = E.create () in
+    let run () =
+      while true do send (fsend ()); Unix.sleep interval done
+    in
+    e, run
   in
-  e, run
+  let _ = E.map frecv seconds in
+  run
 
-let printer = E.map pr_time seconds
-
-let () = run ()
+let () =
+  let r1 = create Unix.gettimeofday pr_time 1 in
+  let r2 = create (fun () -> "Hello!") print_endline 3 in
+  (E.select [r1;r2]) ()
