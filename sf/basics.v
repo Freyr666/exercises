bcc46fc9b53d44f75da864a7f9d2c2abe6124a1a
Freyr <sky_rider_93@mail.ru>
2017-06-09 12:32:52 +0300

    sf: basic done

--- sf/basics.v
+++ sf/basics.v
@@ -228,3 +228,107 @@ Example test_blt_nat2: (blt_nat 2 4) = true.
 Proof. simpl. reflexivity. Qed.
 Example test_blt_nat3: (blt_nat 4 2) = false.
 Proof. simpl. reflexivity. Qed.
+
+Theorem plus_O_n : forall n : nat, 0 + n = n.
+Proof. intros n. simpl. reflexivity. Qed.
+
+Theorem plus_1_l : forall n : nat, 1 + n = S n.
+Proof. intros n. reflexivity. Qed.
+
+Theorem mult_0_l : forall n : nat, 0 * n = 0.
+Proof. intros n. reflexivity. Qed.
+
+Theorem plus_id_example : forall n m : nat,
+    n = m ->
+    n + n = m + m.
+Proof. intros n m. intros H. rewrite <- H. reflexivity. Qed.
+
+Theorem plus_id_exercise : forall n m o : nat,
+  n = m -> m = o -> n + m = m + o.
+Proof. intros n m o. intros H1. intros H2. rewrite -> H1. rewrite -> H2. reflexivity. Qed.
+
+Theorem mult_0_plus : forall n m : nat,
+    (0 + n) * m = n * m.
+Proof. intros n m. rewrite -> plus_O_n. reflexivity. Qed.
+
+Theorem mult_S_l : forall n m : nat,
+    m = S n ->
+    m * (1 + n) = m * m.
+Proof. intros n m. intros H1. rewrite -> plus_1_l. rewrite <- H1. reflexivity. Qed.
+
+Theorem plus_1_neq_0 : forall n : nat,
+    beq_nat (n + 1) 0 = false.
+Proof. intros n. destruct n as [| n'].
+       - reflexivity.
+       - reflexivity. Qed.
+
+Theorem negb_involutive : forall b : bool,
+    negb (negb b) = b.
+Proof.
+  intros b. destruct b.
+  - reflexivity.
+  - reflexivity. Qed.
+
+Theorem andb_commutative : forall b c, andb b c = andb c b.
+Proof.
+  intros b c. destruct b.
+  - destruct c.
+    + reflexivity.
+    + reflexivity.
+  - destruct c.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+Theorem andb_true_elim2 : forall b c : bool,
+    andb b c = true -> c = true.
+Proof.
+  intros b c H. destruct c.
+  - reflexivity.
+  - rewrite <- H. destruct b.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+Theorem zero_nbeq_plus_1 : forall n : nat,
+    beq_nat 0 (n + 1) = false.
+Proof. intros n.
+       destruct n as [| n'].
+       - simpl. reflexivity.
+       - simpl. reflexivity.
+Qed.
+
+Theorem identity_fn_applied_twice :
+  forall(f : bool -> bool), (forall(x : bool), f x = x) ->
+                forall(b : bool), f (f b) = b.
+Proof. intros. destruct b.
+       - rewrite H. rewrite H. reflexivity.
+       - rewrite H. rewrite H. reflexivity.
+Qed.
+
+Theorem negation_fn_applied_twice :
+  forall(f : bool -> bool), (forall(x : bool), f x = negb x) ->
+                forall(b : bool), f (f b) = b.
+Proof. intros. destruct b.
+       - rewrite H. rewrite H. rewrite negb_involutive. reflexivity.
+       - rewrite H. rewrite H. rewrite negb_involutive. reflexivity.
+Qed.
+
+Theorem andb_eq_org :
+  forall(b c : bool), (andb b c = orb b c) -> b = c.
+Proof. intros b c. destruct b.
+       - simpl. intros H. rewrite H. reflexivity.
+       - simpl. intros H. rewrite H. reflexivity.
+Qed.
+
+Inductive bin : Type :=
+| Z : bin
+| D : bin -> bin
+| I : bin -> bin.
+
+Definition incr (n : bin) : bin :=
+  match n with
+  | Z => (I Z)
+  | D n' => (I n)
+  | I n' => (I n)
+  end.
