a2004cf65985a7067cc82498e249d92670a8b0c5
freyr <sky_rider_93@mail.ru>
2017-07-17 00:38:51 +0300

    sf/Logic

--- sf/Logic.v
+++ sf/Logic.v
@@ -56,4 +56,306 @@ Proof.
   reflexivity.
 Qed.
 
+Lemma and_example3 :
+  forall n m : nat, n + m = 0 -> n * m = 0.
+Proof.
+  intros n m H.
+  assert (H' : n = 0 /\ m = 0).
+  { apply and_exercise. apply H. }
+  destruct H' as [Hn Hm].
+  rewrite Hn. reflexivity.
+Qed.
 
+Lemma proj1 : forall P Q : Prop,
+  P /\ Q -> P.
+Proof.
+  intros P Q [HP HQ].
+  apply HP. Qed.
+
+Lemma proj2 : forall P Q : Prop,
+  P /\ Q -> Q.
+Proof.
+  intros P Q [HP HQ].
+  apply HQ. Qed.
+
+Theorem and_commut : forall P Q : Prop,
+  P /\ Q -> Q /\ P.
+Proof.
+  (* WORKED IN CLASS *)
+  intros P Q [HP HQ].
+  split.
+    - (* left *) apply HQ.
+    - (* right *) apply HP. Qed.
+
+Theorem and_assoc : forall P Q R : Prop,
+  P /\ (Q /\ R) -> (P /\ Q) /\ R.
+Proof.
+  intros P Q R [HP [HQ HR]].
+  split.
+  - split.
+    + apply HP.
+    + apply HQ.
+  - apply HR.
+Qed.
+
+Lemma or_example :
+  forall n m : nat, n = 0 \/ m = 0 -> n * m = 0.
+Proof.
+  (* This pattern implicitly does case analysis on
+     n = 0 ∨ m = 0 *)
+  intros n m [Hn | Hm].
+  - (* Here, n = 0 *)
+    rewrite Hn. reflexivity.
+  - (* Here, m = 0 *)
+    rewrite Hm. rewrite <- mult_n_O.
+    reflexivity.
+Qed.
+
+Lemma or_intro : forall A B : Prop, A -> A \/ B.
+Proof.
+  intros A B HA.
+  left.
+  apply HA.
+Qed.
+
+Lemma zero_or_succ :
+  forall n : nat, n = 0 \/ n = S (pred n).
+Proof.
+  intros [|n].
+  - left. reflexivity.
+  - right. reflexivity.
+Qed.
+
+Lemma mult_eq_0 :
+  forall n m, n * m = 0 -> n = 0 \/ m = 0.
+Proof.
+  intros n m H.
+  destruct n.
+  - left. reflexivity.
+  - destruct m.
+    + right. reflexivity.
+    + inversion H.
+Qed.
+  
+Theorem or_commut : forall P Q : Prop,
+  P \/ Q -> Q \/ P.
+Proof.
+  intros P Q [HP | HQ].
+  - right. apply HP.
+  - left. apply HQ.
+Qed.
+
+Theorem ex_falso_quodlibet : forall(P:Prop),
+  False -> P.
+Proof.
+  (* WORKED IN CLASS *)
+  intros P contra.
+  destruct contra. Qed.
+
+Fact not_implies_our_not : forall(P:Prop),
+  ~P -> (forall(Q:Prop), P -> Q).
+Proof.
+  intros P HNP Q HP.
+  destruct HNP. apply HP.
+Qed.
+
+Theorem zero_not_one : ~(0 = 1).
+Proof.
+  intros contra. inversion contra.
+Qed.
+
+Theorem zero_not_one' : 0 <> 1.
+Proof.
+  intros H. inversion H.
+Qed.
+
+Theorem not_False :
+  ~ False.
+Proof.
+  unfold not. intros H. destruct H. Qed.
+
+Theorem contradiction_implies_anything : forall P Q : Prop,
+  (P /\ ~P) -> Q.
+Proof.
+  (* WORKED IN CLASS *)
+  intros P Q [HP HNA]. unfold not in HNA.
+  apply HNA in HP. destruct HP. Qed.
+
+Theorem double_neg : forall P : Prop,
+  P -> ~~P.
+Proof.
+  (* WORKED IN CLASS *)
+  intros P H. unfold not. intros G. apply G. apply H. Qed.
+
+Theorem contrapositive : forall(P Q : Prop),
+  (P -> Q) -> (~Q -> ~P).
+Proof.
+  intros P Q H. unfold not. intros HQ HP.
+  apply HQ in H. destruct H. apply HP.
+Qed.
+  
+Theorem not_both_true_and_false : forall P : Prop,
+  ~ (P /\ ~P).
+Proof.
+  intros P.
+  unfold not. intros [HP HNP]. apply HNP in HP. destruct HP.
+Qed.
+
+Theorem not_true_is_false : forall b : bool,
+  b <> true -> b = false.
+Proof.
+  intros [] H.
+  - (* b = true *)
+    unfold not in H.
+    apply ex_falso_quodlibet.
+    apply H. reflexivity.
+  - (* b = false *)
+    reflexivity.
+Qed.
+
+Theorem not_true_is_false' : forall b : bool,
+  b <> true -> b = false.
+Proof.
+  intros [] H.
+  - (* b = false *)
+    unfold not in H.
+    exfalso. (* <=== *)
+    apply H. reflexivity.
+  - (* b = true *) reflexivity.
+Qed.
+
+Module MyIff.
+
+  Definition iff (P Q : Prop) := (P -> Q) /\ (Q -> P).
+
+  Notation "P <-> Q" := (iff P Q)
+                        (at level 95, no associativity)
+                      : type_scope.
+
+End MyIff.
+
+
+Theorem iff_sym : forall P Q : Prop,
+  (P <-> Q) -> (Q <-> P).
+Proof.
+  (* WORKED IN CLASS *)
+  intros P Q [HAB HBA].
+  split.
+  - (* -> *) apply HBA.
+  - (* <- *) apply HAB. Qed.
+
+Lemma not_true_iff_false : forall b,
+  b <> true <-> b = false.
+Proof.
+  (* WORKED IN CLASS *)
+  intros b. split.
+  - (* -> *) apply not_true_is_false.
+  - (* <- *)
+    intros H. rewrite H. intros H'. inversion H'.
+Qed.
+
+Theorem iff_refl : forall P : Prop,
+  P <-> P.
+Proof.
+  intros P. split.
+  - intros. apply H.
+  - intros. apply H.
+Qed.
+  
+Theorem iff_trans : forall P Q R : Prop,
+  (P <-> Q) -> (Q <-> R) -> (P <-> R).
+Proof.
+  intros P Q R [PQ QP] [QR RQ].
+  split.
+  - intros. apply PQ in H. apply QR in H. apply H.
+  - intros. apply RQ in H. apply QP in H. apply H.
+Qed.
+Theorem or_distributes_over_and : forall P Q R : Prop,
+  P \/ (Q /\ R) <-> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R. split.
+  - intros [HP | [HQ HR]].
+    + split.
+      * left. apply HP.
+      * left. apply HP.
+    + split.
+      * right. apply HQ.
+      * right. apply HR.
+  - intros [[HPR | HQ] [HPL | HR]].
+    + left. apply HPR.
+    + left. apply HPR.
+    + left. apply HPL.
+    + right. split.
+      * apply HQ.
+      * apply HR.
+Qed.
+
+Require Import Coq.Setoids.Setoid.
+
+Lemma mult_0 : forall n m, n * m = 0 <-> n = 0 \/ m = 0.
+Proof.
+  split.
+  - apply mult_eq_0.
+  - apply or_example.
+Qed.
+
+Lemma or_assoc :
+  forall P Q R : Prop, P \/ (Q \/ R) <-> (P \/ Q) \/ R.
+Proof.
+  intros P Q R. split.
+  - intros [H | [H | H]].
+    + left. left. apply H.
+    + left. right. apply H.
+    + right. apply H.
+  - intros [[H | H] | H].
+    + left. apply H.
+    + right. left. apply H.
+    + right. right. apply H.
+Qed.
+
+Lemma mult_0_3 :
+  forall n m p, n * m * p = 0 <-> n = 0 \/ m = 0 \/ p = 0.
+Proof.
+  intros n m p.
+  rewrite mult_0. rewrite mult_0. rewrite or_assoc.
+  reflexivity.
+Qed.
+
+Lemma apply_iff_example :
+  forall n m : nat, n * m = 0 -> n = 0 \/ m = 0.
+Proof.
+  intros n m H. apply mult_0. apply H.
+Qed.
+
+Lemma four_is_even : exists n : nat, 4 = n + n.
+Proof.
+  exists 2. reflexivity.
+Qed.
+
+Theorem exists_example_2 : forall n,
+  (exists m, n = 4 + m) ->
+  (exists o, n = 2 + o).
+Proof.
+  (* WORKED IN CLASS *)
+  intros n [m Hm]. (* note implicit destruct here *)
+  exists(2 + m).
+  apply Hm. Qed.
+
+Theorem dist_not_exists : forall(X:Type) (P : X -> Prop),
+  (forall x, P x) -> ~ (exists x, ~ P x).
+Proof.
+  intros.
+  unfold not. intros NH. destruct NH. apply H0. apply H.
+Qed.
+
+Theorem dist_exists_or : forall (X:Type) (P Q : X -> Prop),
+    (exists x, P x \/ Q x) <-> (exists x, P x) \/ (exists x, Q x).
+Proof.
+  intros X P Q. split.
+  - intros [x [HP | HQ]].
+    + left. exists x. apply HP.
+    + right. exists x. apply HQ.
+  - intros [[x HP] | [x HQ]].
+    + exists x. left. apply HP.
+    + exists x. right. apply HQ.
+Qed.
