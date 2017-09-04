04c1bf85f2c8ff24af67abfa3fea669dc12d1feb
freyr <sky_rider_93@mail.ru>
2017-09-04 22:51:02 +0300

    sf moved to ./sf

---
+++ sf/make.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+coqc Basics.v
+coqc Induction.v
+coqc Lists.v
+coqc Poly.v
+coqc Tactics.v
+coqc Logic.v
+coqc IndProp.v
+coqc Maps.v
+coqc ProofObjects.v
+coqc IndPrinciples.v
+coqc Rel.v
